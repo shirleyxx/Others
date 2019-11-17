337dcbe57ea7d84f6f3c7145713fa28b06e36712
shirleyxx <30725505+shirleyxx@users.noreply.github.com>
2019-11-17 14:18:32 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 def timing(my_function):
     def wrapper():
@@ -8,13 +9,18 @@ def timing(my_function):
         print('\n{} takes {:.4f} s'.format(my_function.__name__, t2 - t1))
     return wrapper
 
+def full_path(path):
+    return os.path.normpath(os.path.realpath(os.path.expanduser(path)))
+
 if __name__=='__main__':
     
     @timing
-    def Test():
+    def test():
         s = 0
         for i in range(100000):
             s = i
         return i
     
     test()
+    
+    print(full_path(__file__))
